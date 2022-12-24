3359209d2d391af235f23faa004881abdb87c533
takehirodesuyo <takehiro.kt106@gmail.com>
2022-12-24 13:16:27 +0900

    router 作成

---
+++ pages/app.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { onMounted } from "@vue/runtime-core";
+import { ref } from "vue";
+
+const message = "Hello";
+</script>
+
+<template>
+  <div>
+    <div>{{ message }}</div>
+    <router-link to="/">index</router-link>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const index = "index";
+</script>
+
+<template>
+  <div>
+    <div>{{ index }}</div>
+    <h1>router</h1>
+    <router-link to="/app">app page</router-link>
+  </div>
+</template>
