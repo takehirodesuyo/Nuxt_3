7f678fdb224f0a8d5fd814d2d52800ef5943ca4f
takehirodesuyo <takehiro.kt106@gmail.com>
2022-12-30 18:31:17 +0900

    一旦

---
+++ pages/users/[id].vue
@@ -0,0 +1,20 @@
+<script setup>
+const route = useRoute();
+
+const message = "/users/_id.vueを表示";
+const id = route.params.id;
+
+onMounted(() => {
+  validate(id);
+});
+</script>
+
+<template>
+  <section>
+    <h1>
+      {{ message }}
+    </h1>
+    <p>id: {{ $route.params.id }}</p>
+    <p>{{ id }}</p>
+  </section>
+</template>
