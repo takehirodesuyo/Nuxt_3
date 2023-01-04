080ca09d5715ec86da6965be1a218e03e016eb93
takehirodesuyo <takehiro.kt106@gmail.com>
2023-01-04 22:23:47 +0900

    layout 作成

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup>
+console.log("wwwwwwwwww");
+const index = "index";
+</script>
+
+<template>
+  <div>{{ index }}</div>
+</template>
+
+<style>
+html {
+  font-size: 100000px;
+}
+</style>
